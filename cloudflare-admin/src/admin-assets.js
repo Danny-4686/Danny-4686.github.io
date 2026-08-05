a15a5268a52db6c731b8fed5a1bd0b6e5f8fc0cb
Danny4686 <halodanielcruz@gmail.com>
2026-08-05 12:42:35 -0500

    Load Journal publishing feedback UI

--- cloudflare-admin/src/admin-assets.js
+++ cloudflare-admin/src/admin-assets.js
@@ -1,2 +1,7 @@
-export { ADMIN_CSS } from './admin-style.js';
-export { ADMIN_JS } from './admin-script.js';
+import { ADMIN_CSS as BASE_ADMIN_CSS } from './admin-style.js';
+import { ADMIN_JS as BASE_ADMIN_JS } from './admin-script.js';
+import { PUBLISH_FEEDBACK_CSS } from './publish-feedback-style.js';
+import { PUBLISH_FEEDBACK_JS } from './publish-feedback.js';
+
+export const ADMIN_CSS = `${BASE_ADMIN_CSS}\n${PUBLISH_FEEDBACK_CSS}`;
+export const ADMIN_JS = `${BASE_ADMIN_JS}\n${PUBLISH_FEEDBACK_JS}`;
