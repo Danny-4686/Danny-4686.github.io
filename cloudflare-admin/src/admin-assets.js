6900c9802d750def038783bae3ee1b6f8d2aac94
Danny4686 <halodanielcruz@gmail.com>
2026-08-06 00:45:58 -0500

    Bundle tabbed Fresh Abyss media controls

--- cloudflare-admin/src/admin-assets.js
+++ cloudflare-admin/src/admin-assets.js
@@ -3,7 +3,8 @@ import { ADMIN_JS as BASE_ADMIN_JS } from './admin-script.js';
 import { PUBLISH_FEEDBACK_CSS } from './publish-feedback-style.js';
 import { PUBLISH_FEEDBACK_JS } from './publish-feedback.js';
 import { FRESH_ABYSS_ADMIN_CSS } from './fresh-abyss-admin-style.js';
-import { FRESH_ABYSS_ADMIN_JS } from './fresh-abyss-admin.js';
+import { ADMIN_TABS_CSS } from './admin-tabs-style.js';
+import { FRESH_ABYSS_ADMIN_V2_JS } from './fresh-abyss-admin-v2.js';
 
-export const ADMIN_CSS = `${BASE_ADMIN_CSS}\n${PUBLISH_FEEDBACK_CSS}\n${FRESH_ABYSS_ADMIN_CSS}`;
-export const ADMIN_JS = `${BASE_ADMIN_JS}\n${PUBLISH_FEEDBACK_JS}\n${FRESH_ABYSS_ADMIN_JS}`;
+export const ADMIN_CSS = `${BASE_ADMIN_CSS}\n${PUBLISH_FEEDBACK_CSS}\n${FRESH_ABYSS_ADMIN_CSS}\n${ADMIN_TABS_CSS}`;
+export const ADMIN_JS = `${BASE_ADMIN_JS}\n${PUBLISH_FEEDBACK_JS}\n${FRESH_ABYSS_ADMIN_V2_JS}`;
