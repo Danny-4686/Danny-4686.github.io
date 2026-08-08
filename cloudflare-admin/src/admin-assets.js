d630a6cc1d50b9da375f32cf73bcc754dc8b2ddf
Danny4686 <halodanielcruz@gmail.com>
2026-08-08 04:17:42 -0500

    Include site intro admin section

--- cloudflare-admin/src/admin-assets.js
+++ cloudflare-admin/src/admin-assets.js
@@ -5,6 +5,8 @@ import { PUBLISH_FEEDBACK_JS } from './publish-feedback.js';
 import { FRESH_ABYSS_ADMIN_CSS } from './fresh-abyss-admin-style.js';
 import { ADMIN_TABS_CSS } from './admin-tabs-style.js';
 import { FRESH_ABYSS_ADMIN_V2_JS } from './fresh-abyss-admin-v2.js';
+import { SITE_INTRO_ADMIN_CSS } from './site-intro-admin-style.js';
+import { SITE_INTRO_ADMIN_JS } from './site-intro-admin-ui.js';
 
-export const ADMIN_CSS = `${BASE_ADMIN_CSS}\n${PUBLISH_FEEDBACK_CSS}\n${FRESH_ABYSS_ADMIN_CSS}\n${ADMIN_TABS_CSS}`;
-export const ADMIN_JS = `${BASE_ADMIN_JS}\n${PUBLISH_FEEDBACK_JS}\n${FRESH_ABYSS_ADMIN_V2_JS}`;
+export const ADMIN_CSS = `${BASE_ADMIN_CSS}\n${PUBLISH_FEEDBACK_CSS}\n${FRESH_ABYSS_ADMIN_CSS}\n${ADMIN_TABS_CSS}\n${SITE_INTRO_ADMIN_CSS}`;
+export const ADMIN_JS = `${BASE_ADMIN_JS}\n${PUBLISH_FEEDBACK_JS}\n${FRESH_ABYSS_ADMIN_V2_JS}\n${SITE_INTRO_ADMIN_JS}`;
